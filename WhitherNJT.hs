becba82daca129c8a3aa4307e86aa64e62fcd094
mark on bitbucket <mark@notarealdomain.dk>
2016-10-22 18:09:10 +0000

    Add the passThrough property to stations. I will need it for NJT.

--- WhitherNJT.hs
+++ WhitherNJT.hs
@@ -25,36 +25,36 @@ module Main where
     | stop == "Gladstone" || stop == "MSU" = Finished
     | otherwise                 = EarlyExit "Got to Hackettstown then went astray."
 
-  allStations = map (\(s,c) -> Station s c) [ 
-    ("Port Jervis", "1:123"),
-    ("Suffern", "1:144"),
-    ("Hackettstown", "1:54"),
-    ("Gladstone", "1:49"),
-    ("New York Penn", "1:105"),
-    ("High Bridge", "1:60"),
-    ("Bay Head", "1:13"),
-    ("Spring Valley", "1:142"),
-    ("Pearl River", "1:118"),
-    ("Trenton", "1:148"),
-    ("Princeton", "1:124"),
-    ("Radburn", "1:126"),
-    ("Clifton", "1:29"),
-    ("MSU", "1:38081")]
+  allStations = map (\(s,c,p) -> Station s c p) [ 
+    ("Port Jervis", "1:123", False),
+    ("Suffern", "1:144", False),
+    ("Hackettstown", "1:54", False),
+    ("Gladstone", "1:49", False),
+    ("New York Penn", "1:105", False),
+    ("High Bridge", "1:60", False),
+    ("Bay Head", "1:13", False),
+    ("Spring Valley", "1:142", False),
+    ("Pearl River", "1:118", False),
+    ("Trenton", "1:148", False),
+    ("Princeton", "1:124", False),
+    ("Radburn", "1:126", True),
+    ("Clifton", "1:29", True),
+    ("MSU", "1:38081", True)]
 
   requiredDestinations :: Set.Set Station
   requiredDestinations = Set.fromList $ filter (\x -> any (== (name x)) [
-    "Suffern",
-    "Hackettstown",
-    "Gladstone",
-    "New York Penn",
-    "High Bridge",
     "Bay Head",
+    "Clifton",
+    "Suffern",
+    "Gladstone",
+    "Hackettstown",
+    "High Bridge",
+    "MSU",
+    "New York Penn",
     "Pearl River",
-    "Trenton",
     "Princeton",
     "Radburn",
-    "Clifton",
-    "MSU"]) allStations
+    "Trenton"]) allStations
 
 
   getStation :: String -> Station
@@ -67,9 +67,9 @@ module Main where
 
   myFaves = tryPermutations allNJTests  requiredDestinations (3 * factorial 10 + 4 * factorial 9 + 6 * factorial 8 + 6 * factorial 7 + 2 * factorial 5 + 1 * factorial 4) (factorial 4)
 
-  myStartTime = UTCTime (fromGregorian 2016 9 27) (8*60*60) -- 8 AM UTC = 4 AM EDT
+  myStartTime = UTCTime (fromGregorian 2016 10 20) (8*60*60) -- 8 AM UTC = 4 AM EDT
   myDeadline = addUTCTime (60 * 60 * 25) myStartTime
 
   -- mainTSP otp test startTime deadline set startIndex numToTry
   main :: IO()
-  main = mainTSP defaultOTP allNJTests myStartTime myDeadline requiredDestinations
+  main = mainTSP defaultOTP allNJTests defaultPlanFlags myStartTime myDeadline requiredDestinations
