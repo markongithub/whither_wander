db2a876a7b9e90499b42de482c489b46bb949771
mark on github <markongithub@users.noreply.github.com>
2023-09-02 11:01:18 -0400

    comment the Millbrae changes

--- src/WhitherBART.hs
+++ src/WhitherBART.hs
@@ -16,6 +16,8 @@ module Main where
 --    , ("Millbrae", "1:MLBR")
     ]
 
+  -- If I arrive at SFO on the Yellow, I have to leave on the Red, and vice
+  -- versa. That way I cross all three legs of the wye.
   sfoFlags :: PlanFlagMaker
   sfoFlags state
     | not atSFO = []
@@ -37,8 +39,10 @@ module Main where
   -- BART is small so this does not matter.
   -- myDeadline = addUTCTime (60 * 60 * 25) myStartTime
 
-  millbraeTest1 = twoAdjacent (getStation "San Francisco International Airport") (getStation "Millbrae")
-  millbraeTest2 = setNotAtBeginningOrEnd (Set.fromList (map getStation ["San Francisco International Airport", "Millbrae"])) (length requiredDestinations)
+  -- Millbrae and SFO used to be two separate termini, but now Millbrae is
+  -- treated as a stop on the Red line between SFO and San Bruno.
+  -- millbraeTest1 = twoAdjacent (getStation "San Francisco International Airport") (getStation "Millbrae")
+  -- millbraeTest2 = setNotAtBeginningOrEnd (Set.fromList (map getStation ["San Francisco International Airport", "Millbrae"])) (length requiredDestinations)
   fremontTest = twoAdjacent (getStation "Berryessa / North San Jose") (getStation "Dublin/Pleasanton")
   allBARTTests = fremontTest -- chainTests millbraeTest1 $ chainTests millbraeTest2 fremontTest
 
